a89047d905303d5af5cc1e78faa72ff114ef9ca4
yourwelcomematt <mattding8@gmail.com>
2021-11-22 17:09:21 +1300

    Created TaxCalculator branch and constructor

---
+++ src/com/matt/payslip/TaxCalculator.java
@@ -0,0 +1,15 @@
+package com.matt.payslip;
+
+public class TaxCalculator {
+
+    private int annualSalary;
+    private int superRate;
+
+    public TaxCalculator(int annualSalary, int superRate) {
+
+        this.annualSalary = annualSalary;
+        this.superRate = superRate;
+
+    }
+
+}
