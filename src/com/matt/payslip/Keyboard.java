cccd2de5e997907f2f612b4c013e2de1fbb0b178
yourwelcomematt <mattding8@gmail.com>
2021-11-22 16:58:28 +1300

    Removed Keyboard class and PayslipApp now takes user input

--- src/com/matt/payslip/Keyboard.java
+++
@@ -1,9 +0,0 @@
-package com.matt.payslip;
-
-import java.util.Scanner;
-
-public class Keyboard {
-
-    private static Scanner keyboardInput = new Scanner(System.in);
-
-}
--- src/com/matt/payslip/PayslipApp.java
+++ src/com/matt/payslip/PayslipApp.java
@@ -1,18 +1,34 @@
 package com.matt.payslip;
 
+import java.util.Scanner;
+
 public class PayslipApp {
 
-    public String firstName;
-    private String surname;
-    private int annualSalary;
-    private int superRate;
-    private String paymentStartDate;
-    private String paymentEndDate;
+    private static Scanner keyboardInput = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to the payslip generator!");
         System.out.println();
+
+        System.out.print("Please input your name: ");
+        String firstName = keyboardInput.nextLine();
+
+        System.out.print("Please input your surname: ");
+        String surname = keyboardInput.nextLine();
+
+        System.out.print("Please enter your annual salary: ");
+        int annualSalary = keyboardInput.nextInt();
+
+        System.out.print("Please enter your super rate: ");
+        int superRate = keyboardInput.nextInt();
+
+        System.out.print("Please enter your payment start date: ");
+        keyboardInput.nextLine();
+        String paymentStartDate = keyboardInput.nextLine();
+
+        System.out.print("Please enter your payment end date: ");
+        String paymentEndDate = keyboardInput.nextLine();
 
     }
 
