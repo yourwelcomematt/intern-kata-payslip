43d7baefd991e32f38dbe28e0939cf3075515a28
yourwelcomematt <mattding8@gmail.com>
2021-11-22 16:11:43 +1300

    Initialised fields in PayslipApp

--- src/com/matt/payslip/PayslipApp.java
+++ src/com/matt/payslip/PayslipApp.java
@@ -2,9 +2,17 @@ package com.matt.payslip;
 
 public class PayslipApp {
 
+    private String firstName;
+    private String surname;
+    private int annualSalary;
+    private int superRate;
+    private String paymentStartDate;
+    private String paymentEndDate;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to the payslip generator!");
+        
 
     }
 
