cc8f63f64484acbb1950a219e2c7d004114def3c
yourwelcomematt <mattding8@gmail.com>
2021-11-22 16:04:06 +1300

    Created PayslipApp class

---
+++ src/com/matt/payslip/PayslipApp.java
@@ -0,0 +1,11 @@
+package com.matt.payslip;
+
+public class PayslipApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the payslip generator!");
+
+    }
+
+}
